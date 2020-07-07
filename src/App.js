80e6c9084abb1e9dc616e1a93b4f59950a867550
raviteja8k <raviteja.tk@gmail.com>
2020-07-07 13:06:12 +0530

    set up Component Tree

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
 
+const ComponentA = () => <ComponentB />;
+const ComponentB = () => <ComponentC />;
+const ComponentC = () => <div>Component C here!</div>;
+
 class App extends React.Component {
   render(){
   return (
     <div className="App">
       <h1>Hola! Simple useContext App</h1>
+      <ComponentA />
     </div>
   );
   }
