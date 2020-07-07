f00afe6f47ecb10a67ad7c16d379e981e01d3b3e
raviteja8k <raviteja.tk@gmail.com>
2020-07-07 14:00:07 +0530

    initiating createContext

--- src/App.js
+++ src/App.js
@@ -1,21 +1,43 @@
-import React from 'react';
+import React, { createContext} from 'react';
 import './App.css';
+
+const {Provider, Consumer} = createContext({
+  user: {},
+  sampleFunc: () => {}
+});
 
 const ComponentA = (props) => <ComponentB user={props.userName}/>;
 const ComponentB = (props) => <ComponentC user={props.user}/>;
-const ComponentC = (props) => <div>Component C here! Username: {props.user}</div>;
+const ComponentC = (props) => <div>Component C here! Username: {props.user.firstName}</div>;
 
 class App extends React.Component {
   constructor(props) {
     super(props);
   
     this.state = {
-      user: 'Ravi'
+      user: {
+        "id": 1,
+        "userName": "ravi2101",
+        "firstName": "Ravi Teja",
+        "lastName": "T" 
+      },
+      sampleFunc: () => this.sampleFunc()
     };
   }
 
+  // componentDidMount(){
+  //   fetch('http://localhost:3000/user')
+  //   .then(res => res.json())
+  //   .then(user => console.log(user))
+    
+  // }
+
+  sampleFunc(){
+    console.log('Sample Func called !');
+  }
+
   render(){
-  return (
+  return (    
     <div className="App">
       <h1>Hola! Simple useContext App</h1>
       <ComponentA userName={this.state.user}/>
