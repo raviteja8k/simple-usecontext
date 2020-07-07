06e714f01ae1bd37afb7ffc9a93b23ca19c9dc84
raviteja8k <raviteja.tk@gmail.com>
2020-07-07 13:10:54 +0530

    set up basic props drilling

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import './App.css';
 
-const ComponentA = () => <ComponentB />;
-const ComponentB = () => <ComponentC />;
-const ComponentC = () => <div>Component C here!</div>;
+const ComponentA = (props) => <ComponentB user={props.userName}/>;
+const ComponentB = (props) => <ComponentC user={props.user}/>;
+const ComponentC = (props) => <div>Component C here! Username: {props.user}</div>;
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+  
+    this.state = {
+      user: 'Ravi'
+    };
+  }
+
   render(){
   return (
     <div className="App">
       <h1>Hola! Simple useContext App</h1>
-      <ComponentA />
+      <ComponentA userName={this.state.user}/>
     </div>
   );
   }
